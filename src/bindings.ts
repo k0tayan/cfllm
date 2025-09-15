d9e05b96263b8e6ccf62a70e0030e52233ae38ce
k0tayan <kotayan0415@outlook.jp>
2025-09-15 15:57:40 +0900

    chore: index.tsをリファクタ

---
+++ src/bindings.ts
@@ -0,0 +1,8 @@
+// Types for bindings from wrangler.toml
+export type Bindings = {
+  DISCORD_PUBLIC_KEY: string;
+  DISCORD_APPLICATION_ID: string;
+  DISCORD_BOT_TOKEN: string;
+  AI: any;
+};
+
---
+++ src/config.ts
@@ -0,0 +1,3 @@
+// Guild allowlist policy
+export const ALLOWED_GUILD_IDs = ['954675852912787476', '826084795720794122'];
+
